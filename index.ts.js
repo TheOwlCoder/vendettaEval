8e4bed96df5c86beafca16b5851ec97353b65c75
TheOwlCoder <100481826+TheOwlCoder@users.noreply.github.com>
2024-09-15 21:55:15 -0500

    Add files via upload

---
+++ index.ts.js
@@ -0,0 +1,156 @@
+import { commands } from "@vendetta";
+import { findByProps } from "@vendetta/metro";
+
+const MessageActions = findByProps("sendMessage", "receiveMessage");
+const Locale = findByProps("Messages");
+
+const endings = [
+    "rawr x3",
+    "OwO",
+    "UwU",
+    "o.O",
+    "-.-",
+    ">w<",
+    "(⑅˘꒳˘)",
+    "(ꈍᴗꈍ)",
+    "(˘ω˘)",
+    "(U ᵕ U❁)",
+    "σωσ",
+    "òωó",
+    "(///ˬ///✿)",
+    "(U ﹏ U)",
+    "( ͡o ω ͡o )",
+    "ʘwʘ",
+    ":3",
+    ":3", // important enough to have twice
+    ":3", // important enough to have thrice
+    "XD",
+    "nyaa~~",
+    "mya",
+    ">_<",
+    "😳",
+    "🥺",
+    "😳😳😳",
+    "rawr",
+    "^^",
+    "^^;;",
+    "(ˆ ﻌ ˆ)♡",
+    "^•ﻌ•^",
+    "/(^•ω•^)",
+    "(✿oωo)"
+];
+
+const replacements = [
+    ["small", "smol"],
+    ["cute", "kawaii"],
+    ["fluff", "floof"],
+    ["love", "luv"],
+    ["stupid", "baka"],
+    ["what", "nani"],
+    ["meow", "nya"],
+    ["hello", "hewwo"],
+];
+
+
+function selectRandomElement(arr) {
+    // generate a random index based on the length of the array
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // return the element at the randomly generated index
+    return arr[randomIndex];
+}
+const isOneCharacterString = (str: string): boolean => {
+    return str.split('').every((char: string) => char === str[0]);
+};
+
+
+function replaceString(inputString) {
+    let replaced = false;
+    for (const replacement of replacements) {
+        const regex = new RegExp(`\\b${replacement[0]}\\b`, "gi");
+        if (regex.test(inputString)) {
+            inputString = inputString.replace(regex, replacement[1]);
+            replaced = true;
+        }
+    }
+    return replaced ? inputString : false;
+}
+
+
+function uwuify(message: string): string {
+    const rule = /\S+|\s+/g;
+    const words: string[] | null = message.match(rule);
+    let answer = "";
+
+    if (words === null) return "";
+
+    for (let i = 0; i < words.length; i++) {
+        if (isOneCharacterString(words[i]) || words[i].startsWith("https://")) {
+            answer += words[i];
+            continue;
+        }
+
+        if (!replaceString(words[i])) {
+            answer += words[i]
+                .replace(/n(?=[aeo])/g, "ny")
+                .replace(/l|r/g, "w");
+        } else answer += replaceString(words[i]);
+
+    }
+
+    answer += " " + selectRandomElement(endings);
+    return answer;
+}
+
+function uwuifyArray(arr) {
+    const newArr = [...arr];
+
+    newArr.forEach((item, index) => {
+        if (Array.isArray(item)) {
+            newArr[index] = uwuifyArray(item);
+        } else if (typeof item === "string") {
+            newArr[index] = uwuify(item);
+        }
+    });
+
+    return newArr;
+}
+
+
+
+
+let patches = [];
+
+export default {
+    onLoad: () => {
+        patches.push(commands.registerCommand({
+            name: "Evaluate",
+            displayName: "Eval",
+            description: "Evaluate Javascript Code",
+            displayDescription: "Evaluate Javascript Code",
+            options: [{
+                name: "code",
+                displayName: "code",
+                description: Locale.Messages.COMMAND_SHRUG_MESSAGE_DESCRIPTION,
+                displayDescription: Locale.Messages.COMMAND_SHRUG_MESSAGE_DESCRIPTION,
+                required: true,
+                // @ts-ignore
+                type: 3
+            }],
+            // @ts-ignore
+            applicationId: -1,
+            inputType: 1,
+            type: 1,
+        
+            execute: (args, ctx) => {
+               
+                MessageActions.sendMessage(ctx.channel.id, {
+                    content: "test message for eval cmd"
+                })
+            }
+        }));
+    },
+    onUnload: () => {
+        for (const unpatch of patches) unpatch()
+    }
+}
